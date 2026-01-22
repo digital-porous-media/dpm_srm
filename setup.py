37ebe1cf11fe9b3312b5448d37160910be35c91f
Bernard Chang <bcchang@utexas.edu>
2026-01-22 15:37:45 -0600

    Update Python versions for wheels and bump package version to 0.1.8

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ ext_modules = [
 
 setup(
     name='dpm_srm',
-    version='0.1.7',
+    version='0.1.8',
     author='Digital Porous Media',
     author_email='bcchang@utexas.edu',
     description='Statistical Region Merging Segmentation',
